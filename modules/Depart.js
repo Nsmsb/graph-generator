674c180a301828a1d78afa0669eb70f0d5fdad4f
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-21 02:34:49 +0100

    adding Depart class, it contains the time of depart an price, it's used in the Adjacent class

---
+++ modules/Depart.js
@@ -0,0 +1,10 @@
+module.exports = class Depart {
+
+    constructor(departTime, price) {
+
+        //getters and setters
+        
+        this.getDepartTime = () => departTime;
+        this.getPrice = () => price;
+    }
+}
