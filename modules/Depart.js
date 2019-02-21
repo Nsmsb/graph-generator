148f7c4d173069b94cfca9ec54dadbdb0162fc0f
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-21 10:37:55 +0100

    added methodes

--- modules/Depart.js
+++ modules/Depart.js
@@ -1,10 +1,12 @@
 module.exports = class Depart {
 
-    constructor(departTime, price) {
+    constructor({departTime, distance, price}) { 
 
         //getters and setters
-        
+
         this.getDepartTime = () => departTime;
+        this.getDistance = () => distance;
         this.getPrice = () => price;
     }
+
 }
