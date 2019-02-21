8d1879f3fa209b63e3e820c06423c3c6096f9572
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-21 09:18:22 +0100

    rename the TranspotMode.js to TransportPlanning.js, it's instances are used inside the Adjacent class

---
+++ modules/TransportPlanning.js
@@ -0,0 +1,17 @@
+module.exports = class TransportPlanning {
+    constructor(path, {walk, bus, metro, tramway}) {
+
+        // getters and setters
+        this.getPath = () => path;
+
+        this.getPlanning = () => {
+            walk,
+            bus,
+            metro,
+            tramway
+        };
+
+        this.getPlan
+    }
+}
+
