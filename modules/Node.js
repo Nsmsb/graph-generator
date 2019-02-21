35097d87a2730f21df7e868147f8ace4b3390e4d
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-21 12:17:58 +0100

    added addAdjacent() method

--- modules/Node.js
+++ modules/Node.js
@@ -1,12 +1,23 @@
-modules.export = class Node {
+const Adjacent =  require('./Adjacent');
+
+module.exports = class Node {
     
     constructor({id, adjacents}) {
 
+        adjacents = adjacents || [];
 
         // setters and getters
         this.getAdjacents = () => adjacents;
         this.getId = () => id;
 
+    }
+
+    addAdjacent(id) {
+        this.getAdjacents().push(
+            new Adjacent({
+                destination: id
+            });
+        )
     }
 
 }
