0a59a3205ff21da21273802442aca6eb60668167
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-21 09:38:29 +0100

    add getPlanningMixed(), it return an array of all Departs available sorted by depart time

--- modules/TransportPlanning.js
+++ modules/TransportPlanning.js
@@ -1,17 +1,31 @@
 module.exports = class TransportPlanning {
     constructor(path, {walk, bus, metro, tramway}) {
 
+        /*
+        walk, bus, metro and tramway are arrays of departs
+        each elements means the mode, time and
+        */
+
+
         // getters and setters
         this.getPath = () => path;
 
         this.getPlanning = () => {
             walk,
-            bus,
+            bus,                            
             metro,
             tramway
         };
 
-        this.getPlan
+        this.getPlanningMixed = () => {
+            
+            return (
+                walk.concat(bus)
+                .concat(metro)
+                .concat(tramway)
+            );
+        };
+
     }
 }
 
