d176afe068960dc19ff46416cdc780f26ab76f87
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-22 17:16:33 +0100

    deleted the distance property, each transport mode from a city i to a city j usally has the same distance, instead i added it to the Planning

--- modules/Depart.js
+++ modules/Depart.js
@@ -1,11 +1,11 @@
 module.exports = class Depart {
 
-    constructor({departTime, distance, price}) { 
+    constructor({departTime, price}) { 
 
         //getters and setters
 
         this.getDepartTime = () => departTime;
-        this.getDistance = () => distance;
+        //this.getDistance = () => distance;
         this.getPrice = () => price;
     }
 
