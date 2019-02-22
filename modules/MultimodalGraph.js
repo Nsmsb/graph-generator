9db68fe424e268a571ef963159e7bff56efbd8dd
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-22 17:44:13 +0100

    added addTransportMode() methode

--- modules/MultimodalGraph.js
+++ modules/MultimodalGraph.js
@@ -1,9 +1,33 @@
 const fs = require('fs');
+const Node = require('./Node');
+const TransportMode = require('./TransportMode');
 
-module.export = class MultimodalGraph {
+module.exports = class MultimodalGraph {
 
-    constructor(graph, transportModes) {
+    constructor({graph, transportModes}) {
 
+        // initializing properties
+        this.nodes = new Array(graph.length);
+        this.transportModes = transportModes || [];
+        
+        for(let i = 0; i < graph.length; i++) {
+
+            // adding a node
+            this.nodes[i] = new Node({
+                id: i
+            });
+
+            // adding its adjacents
+            graph[i].adjacencyList.forEach(adjacent => {
+                this.nodes[i].addAdjacent(adjacent);
+            });
+            
+        }
+
+    }
+
+    addTransportMode({path, departs}) {
+        this.transportModes.push(new TransportMode({path, departs}));
     }
 
     writeToFile(path) {
