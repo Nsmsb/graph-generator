f945cd80c50ec729a3908f7dc034c2508450fbad
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2019-02-24 23:46:26 +0100

    added comments

--- modules/TransportMode.js
+++ modules/TransportMode.js
@@ -1,10 +1,15 @@
 module.exports = class TransportMode {
 
+    /*
+
+    transport modes are used to fill the graph and also are refered to at the departs (vehicle)
+
+    */
     constructor({id, type, path, departs}) {
 
 
         // getters and setters
-        
+
         this.getId = () => id;
         this.getType = () => type;
         this.getPath = () => path;
